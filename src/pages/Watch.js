79dc79536608c40a280c2895d3cbc5fb34e83424
kkosuke <kkosuke007@gmail.com>
2023-02-19 19:08:20 +0900

    videoDetail (#5)

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import VideoDetail from '../components/VideoDetail/VideoDetail'
 
 const Watch = () => {
   return (
     <Layout>
       <div>Watch</div>
+      <VideoDetail />
     </Layout>
   )
 }
