6513d119be2a2739167f3fdf44fc1c6587d38d46
kkosuke <kkosuke007@gmail.com>
2023-02-19 17:25:43 +0900

    youtubeAPI (#2)

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Layout from '../components/Layout/Layout'
+import { fetchPopularData } from '../apis/index'
+
 
 const Top = () => {
+  useEffect(()=>{
+    fetchPopularData().then((res)=>{
+      console.log('fetchPopularData', res);
+    })
+
+  },[])
   return (
     <Layout>
+      <br />
+      <br />
+      <br />
+      <br />
+      {process.env.YOUTUBE_API_KEY}
       <div>Top</div>
     </Layout>
   )
