f9f3ec831139b08c4c77e09acd75f1ede7809fb8
kkosuke <kkosuke007@gmail.com>
2023-02-19 19:07:09 +0900

    videoDetail

--- src/apis/index.js
+++ src/apis/index.js
@@ -5,17 +5,28 @@ const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 const youtube = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3'
 });
-
+const params = {
+  part: 'snippet',
+  maxResults: 40,
+  key: KEY,
+  regionCode: 'JP',
+  type: 'video',
+}
 export const fetchPopularData = async () =>{
   return await youtube.get('/videos',{
     // https://developers.google.com/youtube/v3/docs?hl=ja
     params: {
-      part: 'snippet',
-      maxResults: 40,
-      key: KEY,
-      regionCode: 'JP',
-      type: 'video',
+      ...params,
       chart: 'mostPopular'
+    }
+  })
+}
+
+export const fetchSelectedData = async (id) =>{
+  return await youtube.get('/videos',{
+    params: {
+      ...params,
+      id
     }
   })
 }
