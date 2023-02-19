e12a438faf3dc143d802ca4b135ba808b2cffb21
kkosuke <kkosuke007@gmail.com>
2023-02-19 17:53:53 +0900

    global state (#3)

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,15 +1,23 @@
-import React, {useEffect} from 'react'
+import React, {useContext, useEffect} from 'react'
 import Layout from '../components/Layout/Layout'
 import { fetchPopularData } from '../apis/index'
-
+import { Store } from '../store/index';
 
 const Top = () => {
+  const {setGlobalState} = useContext(Store);
   useEffect(()=>{
     fetchPopularData().then((res)=>{
       console.log('fetchPopularData', res);
+      setGlobalState({
+        type: 'SET_POPULAR',
+        payload: {
+          popular: res.data.items
+        }
+      })
     })
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
+
   return (
     <Layout>
       <br />
