3c11ec838b52aee7f7b2b9a21a3d160ec54217bf
kkosuke <kkosuke007@gmail.com>
2023-02-19 20:43:23 +0900

    ヘッダー検索ワードをglobalStateに

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, {useState, useEffect, useContext} from 'react'
+import {Link, useNavigate} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-
 import Style from "./Header.module.scss"
+import { Store } from '../../store/index';
+
 
 const Header = () => {
+  const {globalState, setGlobalState} = useContext(Store);
+  const [term , setTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setGlobalState({
+      type: "SET_TERM",
+      payload: {
+        term
+      }
+    })
+    navigate(`/search?query=${term}`)
+  }
+  useEffect(()=>{
+    setTerm(globalState.term)
+  },[globalState.term])
+
   return (
     <div className={Style.header}>
       <div className={Style.item}>
         <Link to="/">VideoTube</Link>
       </div>
       <div className={Style.item}>
-        <form>
-          <input type="text" placeholder='検索' />
+        <form onSubmit={e=>handleSubmit(e)}>
+          <input type="text" placeholder='検索' onChange={(e)=>setTerm
+          (e.target.value)}
+          value={term}/>
           <button type="submit">
             <FontAwesomeIcon icon={faSearch} />
           </button>
