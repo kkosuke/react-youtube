8957c3728f3d719936d3499710dd1b41d7e68290
kkosuke <kkosuke007@gmail.com>
2023-02-19 17:24:24 +0900

    youtubeAPI

---
+++ src/apis/index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+// 変数名は「REACT_APP_〜」の形にしないと上手くいきません。
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const youtube = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3'
+});
+
+export const fetchPopularData = async () =>{
+  return await youtube.get('/videos',{
+    // https://developers.google.com/youtube/v3/docs?hl=ja
+    params: {
+      part: 'snippet',
+      maxResult: 40,
+      key: KEY,
+      regionCode: 'JP',
+      type: 'video',
+      chart: 'mostPopular'
+    }
+  })
+}
